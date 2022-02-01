8e6fc98646db9aa2acfc37e78c9fb03cceeafee8
gumberss <gustavomarinsuppi@gmail.com>
2022-01-31 21:03:24 -0300

    chore: removing arrow

--- lib/expenses/screens/Expenses.dart
+++ lib/expenses/screens/Expenses.dart
@@ -96,7 +96,6 @@ class _ExpensesScreenState extends State<ExpensesScreen> {
                 ),
                 Card(
                   child: ListTile(
-                    leading: Icon(Icons.arrow_forward),
                     title: Text("Items Details:"),
                     subtitle: Text(
                         "Item Quantity: ${items.length}     Total Cost: ${items.map((e) => e.amount * e.cost).fold<double>(0, (previousValue, element) => previousValue + element)}"),
