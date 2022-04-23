4eb089ea326f14f8ffb284e037b8f7583f7670e3
gumberss <gustavomarinsuppi@gmail.com>
2022-04-23 15:53:05 -0300

    chore: Adding more functions to default input

--- lib/components/DefaultInput.dart
+++ lib/components/DefaultInput.dart
@@ -1,22 +1,40 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class DefaultInput extends StatelessWidget{
+class DefaultInput extends StatelessWidget {
   String title;
   TextInputType type;
   TextEditingController controller;
 
-  DefaultInput(this.title, this.type, this.controller);
+  bool obscureText;
+
+  FormFieldValidator<String>? validator;
+  ValueChanged<String>? onChanged;
+  AutovalidateMode autoValidateMode;
+
+  DefaultInput(this.title, this.type, this.controller,
+      {this.obscureText = false,
+      this.validator,
+      this.onChanged,
+      this.autoValidateMode = AutovalidateMode.onUserInteraction});
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
-        padding: EdgeInsets.only(left:  8.0, right: 8.0, top: 2.0, bottom: 2.0),
-        child: TextField(
+    return Padding(
+        padding: EdgeInsets.only(left: 8.0, right: 8.0, top: 2.0, bottom: 2.0),
+        child: TextFormField(
+          obscureText: obscureText,
           controller: controller,
-          decoration: InputDecoration(labelText: title, ),
+          onChanged: onChanged,
+          autovalidateMode: autoValidateMode,
+          decoration: InputDecoration(
+              labelText: title,
+              errorBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.redAccent),
+              )),
           style: TextStyle(fontSize: 16.0),
           keyboardType: type,
+          validator: validator,
         ));
   }
 }
