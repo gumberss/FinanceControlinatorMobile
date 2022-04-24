8ca57f0ed49769eb336aee3ddbd480b6d7f28032
gumberss <gustavomarinsuppi@gmail.com>
2022-04-24 15:13:19 -0300

    chore: adding Progress component

---
+++ lib/components/Progress.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class Progress extends StatelessWidget {
+  final String? message;
+
+  Progress({this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children:  [
+          const CircularProgressIndicator(),
+          Padding(
+            padding: const EdgeInsets.only(top: 8),
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: new BoxDecoration(color: Theme.of(context).backgroundColor,
+              borderRadius: BorderRadius.all(Radius.circular(16))),
+              child: Text(
+                message != null ? message! : 'Hang in there :D',
+                style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
