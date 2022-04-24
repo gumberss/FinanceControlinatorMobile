b9078b0441455d460006fc2bcc7d2ab46090d7eb
gumberss <gustavomarinsuppi@gmail.com>
2022-04-24 15:12:39 -0300

    fix: SignUp name and Username

--- lib/authentications/webclients/AuthenticationWebClient.dart
+++ lib/authentications/webclients/AuthenticationWebClient.dart
@@ -3,16 +3,15 @@ import 'dart:io';
 
 import 'package:finance_controlinator_mobile/authentications/domain/SignUpUser.dart';
 import 'package:finance_controlinator_mobile/components/HttpClient/http_client.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-class AuthenticationWebClient {
-  Uri baseUri = Uri.http(
-      dotenv.env['FINANCE_CONTROLINATOR_API_URL'].toString(), "/SignUp");
+import '../domain/SignInUser.dart';
 
-  Future<int> signup(SignUpUser user) async {
-    debugPrint(baseUri.toString());
-    final response = await client.post(baseUri,
+class AuthenticationWebClient {
+  String baseUrl = dotenv.env['FINANCE_CONTROLINATOR_API_URL'].toString();
+
+  Future<int> signUp(SignUpUser user) async {
+    final response = await client.post(Uri.http(baseUrl, "/SignUp"),
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
         },
@@ -25,12 +24,39 @@ class AuthenticationWebClient {
       var decodedJson = jsonDecode(response.body);
       var descriptions = (decodedJson as List)
           .map((e) => e["description"].toString())
-          .map((e) => e.splitMapJoin(RegExp(r'^(.{25}.*?) '), onMatch: (m)=> '${m[0]}\n'))
+          .map((e) => e.splitMapJoin(RegExp(r'^(.{25}.*?) '),
+              onMatch: (m) => '${m[0]}\n'))
           .reduce((value, element) => value += "\n$element")
           .toString();
       throw new HttpException(descriptions);
     }
 
+    return response.statusCode;
+  }
+
+  Future<int> signIn(SignInUser user) async {
+    final response = await client.post(Uri.http(baseUrl, "/SignIn"),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(user.toJson()));
+
+    if (response.statusCode == 500)
+      throw new HttpException("It was not possible to sign in :(");
+
+    if (response.statusCode == 400) {
+      var decodedJson = jsonDecode(response.body);
+      var descriptions = (decodedJson as List)
+          .map((e) => e["description"].toString())
+          .map((e) => e.splitMapJoin(RegExp(r'^(.{25}.*?) '),
+              onMatch: (m) => '${m[0]}\n'))
+          .reduce((value, element) => value += "\n$element")
+          .toString();
+      throw new HttpException(descriptions);
+    }
+    if (response.statusCode == 401) {
+      throw new HttpException("User name or password is invalid");
+    }
     return response.statusCode;
   }
 }
