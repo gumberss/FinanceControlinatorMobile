8f8f5b73f23040f79d40e379f7f19fa1cdcf4c4d
gumberss <gustavomarinsuppi@gmail.com>
2022-04-28 08:52:53 -0300

    chore: Adding AuthorizationService to grap the redirect to sign in logic

---
+++ lib/authentications/services/AuthorizationService.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../screens/SignIn.dart';
+
+class AuthorizationService {
+  static void redirectToSignIn(BuildContext context){
+    @override
+    void run() {
+      scheduleMicrotask(() {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => SignIn()),
+        );
+      });
+    }
+
+    run();
+  }
+}
