12bc50b854b9658db9c2f76fdbfb51423249365f
gumberss <gustavomarinsuppi@gmail.com>
2022-04-29 18:47:33 -0300

    feat: adding jwt in each request. To do it, the best way I think was changing the HttpClient for Dio that already has a interceptor that able me to add the token in the header

--- lib/authentications/webclients/AuthenticationWebClient.dart
+++ lib/authentications/webclients/AuthenticationWebClient.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:dio/dio.dart';
 import 'package:finance_controlinator_mobile/authentications/domain/SignUpUser.dart';
 import 'package:finance_controlinator_mobile/components/HttpClient/http_client.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -11,18 +12,17 @@ class AuthenticationWebClient {
   String baseUrl = dotenv.env['FINANCE_CONTROLINATOR_API_URL'].toString();
 
   Future<int> signUp(SignUpUser user) async {
-    final response = await client.post(Uri.http(baseUrl, "/SignUp"),
-        headers: <String, String>{
+    final response = await client.postUri(Uri.http(baseUrl, "/SignUp"),
+        options: Options(headers: {
           'Content-Type': 'application/json; charset=UTF-8',
-        },
-        body: jsonEncode(user.toJson()));
+        }),
+        data: user.toJson());
 
     if (response.statusCode == 500)
       throw new HttpException("It was not possible to \ncreate the user :(");
 
     if (response.statusCode == 400) {
-      var decodedJson = jsonDecode(response.body);
-      var descriptions = (decodedJson as List)
+      var descriptions = (response.data as List)
           .map((e) => e["description"].toString())
           .map((e) => e.splitMapJoin(RegExp(r'^(.{25}.*?) '),
               onMatch: (m) => '${m[0]}\n'))
@@ -31,15 +31,16 @@ class AuthenticationWebClient {
       throw new HttpException(descriptions);
     }
 
-    return response.statusCode;
+    return response.statusCode!;
   }
 
   Future<String?> signIn(SignInUser user) async {
-    final response = await clientWithoutInterceptor.post(Uri.http(baseUrl, "/SignIn"),
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-        },
-        body: jsonEncode(user.toJson()));
+    final response =
+        await clientWithoutInterceptor.postUri(Uri.http(baseUrl, "/SignIn"),
+            options: Options(headers: {
+              'Content-Type': 'application/json; charset=UTF-8',
+            }),
+            data: user.toJson());
 
     if (response.statusCode == 500)
       throw new HttpException("It was not possible to sign in :(");
@@ -48,8 +49,7 @@ class AuthenticationWebClient {
       throw new HttpException("User name or password is invalid");
     }
 
-    if(response.statusCode == 200)
-      return jsonDecode(response.body)["token"];
+    if (response.statusCode == 200) return response.data["token"];
 
     return null;
   }
--- lib/expenses/webclients/ExpenseWebClient.dart
+++ lib/expenses/webclients/ExpenseWebClient.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:dio/dio.dart';
 import 'package:finance_controlinator_mobile/components/HttpClient/HttpResponseData.dart';
 import 'package:finance_controlinator_mobile/components/HttpClient/http_client.dart';
 import 'package:finance_controlinator_mobile/expenses/domain/Expense.dart';
@@ -15,36 +16,37 @@ class ExpenseWebClient {
       "/api/expenses");
 
   Future<Expense> save(Expense expense) async {
-    final response = await client.post(baseUri,
-        headers: <String, String>{
+    final response = await client.postUri(baseUri,
+        options: Options(headers: {
           'Content-Type': 'application/json; charset=UTF-8',
-        },
-        body: jsonEncode(expense.toJson()));
+        }),
+        data: expense.toJson());
 
-    if (response.statusCode == 500)
-      throw new HttpException("It was not possible to \ncreate the expense :(");
+    if (response.statusCode == 500) {
+      throw const HttpException(
+          "It was not possible to \ncreate the expense :(");
+    }
 
     return expense;
   }
 
   Future<List<Expense>> getPage(int page, int count) async {
-    final response = await client.get(
-        baseUri.replace(path: "api/expenses/$page/$count"),
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-        });
+    final response =
+        await client.getUri(baseUri.replace(path: "api/expenses/$page/$count"),
+            options: Options(headers: {
+              'Content-Type': 'application/json; charset=UTF-8',
+            }));
 
-    debugPrint(response.body);
-
-    if (response.statusCode == 500)
-      throw new HttpException(
+    if (response.statusCode == 500) {
+      throw const HttpException(
           "It was not possible to \nGet the expense list :(");
+    }
 
     if (response.statusCode == 401) {
       return List.empty();
     }
 
-    Iterable expenseDecoded = json.decode(response.body);
+    Iterable expenseDecoded = response.data;
 
     return List<Expense>.from(expenseDecoded.map((e) => Expense.fromJson(e)));
   }
@@ -56,14 +58,17 @@ class ExpenseOverviewWebClient {
       "/api/expenses/overview");
 
   Future<HttpResponseData<ExpenseOverview>> GetOverview() async {
-    final response = await client.get(baseUri);
+    final response = await client.getUri(baseUri);
 
-    if (response.statusCode == 500)
-      throw new HttpException("It was not possible to \nfind the overview :(");
+    if (response.statusCode == 500) {
+      throw const HttpException(
+          "It was not possible to \nfind the overview :(");
+    }
 
-    if(response.statusCode == 401)
-      return HttpResponseData(response.statusCode, null);
-    var expenseOverview = ExpenseOverview.fromJson(jsonDecode(response.body));
-    return HttpResponseData(response.statusCode, expenseOverview);
+    if (response.statusCode == 401) {
+      return HttpResponseData(response.statusCode!, null);
+    }
+    var expenseOverview = ExpenseOverview.fromJson(response.data);
+    return HttpResponseData(response.statusCode!, expenseOverview);
   }
 }
