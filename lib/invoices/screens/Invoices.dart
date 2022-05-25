187632ef214d30723b09c0ae2cb58c62e62f83fe
gumberss <gustavomarinsuppi@gmail.com>
2022-05-24 23:08:44 -0300

    feat: adding the partitions template for invoices

--- lib/invoices/screens/Invoices.dart
+++ lib/invoices/screens/Invoices.dart
@@ -70,7 +70,7 @@ class InvoiceDataScreen extends StatelessWidget {
     return Column(
       children: [
         SizedBox(
-          height: 200,
+          height: 250,
           child: InvoiceOverviewScreen(invoiceMonthData.overview),
         ),
         Padding(
@@ -92,14 +92,22 @@ class InvoiceOverviewScreen extends StatelessWidget {
     return Container(
         color: Colors.lightBlue.shade100,
         alignment: Alignment.topCenter,
-        child: Column(
-          children: [
-            OverviewHeader(
-                overview.date, overview.statusText, overview.totalCost),
-            OverviewBriefs(overview.briefs),
-            OverviewPartitions(),
-          ],
-        ));
+        child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              children: [
+                Padding(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: OverviewHeader(overview.date, overview.statusText,
+                        overview.totalCost)),
+                Padding(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: OverviewBriefs(overview.briefs)),
+                Padding(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: OverviewPartitions(overview.partitions)),
+              ],
+            )));
   }
 }
 
@@ -108,25 +116,22 @@ class OverviewHeader extends StatelessWidget {
   String statusText;
   String totalCost;
 
-  OverviewHeader(this.date, this.statusText, this.totalCost, {Key? key}) : super(key: key);
+  OverviewHeader(this.date, this.statusText, this.totalCost, {Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(8),
-      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-        Text(date,
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-        Text(statusText),
-        Text(totalCost,
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20))
-      ]),
-    );
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+      Text(date,
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+      Text(statusText),
+      Text(totalCost,
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20))
+    ]);
   }
 }
 
 class OverviewBriefs extends StatelessWidget {
-
   List<InvoiceBrief> briefs;
 
   OverviewBriefs(this.briefs, {Key? key}) : super(key: key);
@@ -147,10 +152,100 @@ class OverviewBriefs extends StatelessWidget {
 }
 
 class OverviewPartitions extends StatelessWidget {
+  List<InvoicePartition> partitions;
+
+  OverviewPartitions(this.partitions, {Key? key}) : super(key: key);
+
+  List<Color> colors = [
+    Colors.redAccent,
+    Colors.greenAccent,
+    Colors.blueAccent,
+    Colors.orangeAccent,
+    Colors.blueGrey,
+    Colors.purpleAccent,
+    Colors.deepOrangeAccent,
+  ];
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Text("data");
+    var descriptions = buildDescriptions(partitions, colors);
+
+    return Column(
+      children: [
+        Padding(
+            padding: const EdgeInsets.only(bottom: 8),
+            child: buildBar(partitions, colors)),
+        Padding(
+            padding: const EdgeInsets.only(bottom: 8),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: descriptions.take(4).toList())),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: descriptions.skip(4).take(4).toList())
+      ],
+    );
+  }
+
+  List<Row> buildDescriptions(
+      List<InvoicePartition> parts, List<Color> colors) {
+    return parts
+        .asMap()
+        .map((key, value) => MapEntry(
+            key,
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 4, right: 4),
+                  child: Text(value.typeText + ":"),
+                ),
+                Container(
+                    height: 20,
+                    width: 20,
+                    decoration: BoxDecoration(
+                        color: colors[key],
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(100))))
+              ],
+            )))
+        .values
+        .toList();
+  }
+
+//todo: make a component
+  Row buildBar(List<InvoicePartition> parts, List<Color> colors) {
+    var partitionsWithSpend =
+        parts.where((element) => element.totalValue > 0).toList();
+
+    return Row(
+        children: partitionsWithSpend
+            .asMap()
+            .map((key, value) {
+              return MapEntry(
+                  key,
+                  Expanded(
+                      flex: value.percent.toInt(),
+                      child: Container(
+                        height: 20,
+                        decoration: BoxDecoration(
+                            color: colors[key],
+                            borderRadius:
+                                getBorder(key, partitionsWithSpend.length - 1)),
+                      )));
+            })
+            .values
+            .toList());
+  }
+
+//todo: be part of the component
+  BorderRadius getBorder(int itemPosition, int lastPosition) {
+    return itemPosition == 0
+        ? const BorderRadius.only(
+            topLeft: Radius.circular(6), bottomLeft: Radius.circular(6))
+        : itemPosition == lastPosition
+            ? const BorderRadius.only(
+                topRight: Radius.circular(6), bottomRight: Radius.circular(6))
+            : const BorderRadius.all(Radius.zero);
   }
 }
 
