4f70d472e537d5da7d5e5eed36e8ce400445c7b9
gumberss <gustavomarinsuppi@gmail.com>
2022-05-29 22:11:06 -0300

    feat: adding invoice item component

--- lib/invoices/screens/Invoices.dart
+++ lib/invoices/screens/Invoices.dart
@@ -5,6 +5,7 @@ import 'package:finance_controlinator_mobile/invoices/services/InvoiceSyncServic
 import 'package:finance_controlinator_mobile/invoices/services/SyncStorageService.dart';
 
 import 'package:flutter/material.dart';
+import '../../expenses/components/InfiniteList.dart';
 import '../domain/sync/InvoiceSync.dart';
 import '../webclients/InvoiceSyncWebClient.dart';
 import 'InvoiceOverview.dart';
@@ -98,4 +99,73 @@ class InvoiceDataScreen extends StatelessWidget {
       ],
     );
   }
+}
+
+class InvoiceItensComponent extends StatelessWidget {
+
+  List<InvoiceItem> items;
+
+  InvoiceItensComponent(this.items, {Key? key}) : super(key: key);
+
+  Future<List<InvoiceItem>> requestItems(int page, int itemsCount) async {
+    return items.skip(page * itemsCount).take(itemsCount).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: RefreshIndicator(
+        onRefresh: () => requestItems(1, 10),
+        child: InifiniteList<InvoiceItem>(
+          onRequest: requestItems,
+          itensPerPage: 10,
+          itemBuilder: (context, item, index) => Container(
+              height: 100,
+              decoration: BoxDecoration(
+                  color: index % 2 == 0
+                      ? Colors.lightBlue.shade50
+                      : Colors.lightBlue.shade100,
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(.6),
+                      spreadRadius: 1,
+                      blurRadius: 2,
+                      offset: const Offset(2, 2), // changes position of shadow
+                    )
+                  ]),
+              margin:
+              const EdgeInsets.only(top: 8, bottom: 8, left: 4, right: 4),
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        item.id,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      Text("Parcelas: ${item.id}")
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      //Text("R\$ ${item.totalCost.toStringAsFixed(2)}"),
+                      //Text("Data da compra: ${DateFormat("dd-MM-yyyy").format(item.purchaseDate)}"),
+                    ],
+                  )
+                ],
+              )),
+        ),
+      ),
+    );
+  }
+
 }
