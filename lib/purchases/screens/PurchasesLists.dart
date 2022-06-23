2fb93cc06e13fc2e2d6f27bc650ecb9b2c39bcdd
gumberss <gustavomarinsuppi@gmail.com>
2022-06-23 08:50:43 -0300

    feat: creating purchases lists screen #49

---
+++ lib/purchases/screens/PurchasesLists.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PurchasesListsScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Purchases Lists"), //from dto
+      ),
+      body: Text("Body"),
+    );
+  }
+}
