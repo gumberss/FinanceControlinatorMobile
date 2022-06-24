8459234d0ba6116bcc8f4befbe81f697d20d71bf
gumberss <gustavomarinsuppi@gmail.com>
2022-06-24 20:04:07 -0300

    feat: starting add purchase list button

--- lib/purchases/screens/PurchasesLists.dart
+++ lib/purchases/screens/PurchasesLists.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class PurchasesListsScreen extends StatelessWidget{
+import '../../components/DefaultDialog.dart';
+
+class PurchasesListsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Purchases Lists"), //from dto
-      ),
-      body: Text("Body"),
-    );
+        appBar: AppBar(
+          title: Text("Purchases Lists"), //from dto
+        ),
+        body: Text("Body"),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.blueAccent,
+          child: Icon(Icons.add),
+          onPressed: () => {
+            DefaultDialog().showDialog(context)
+          },
+        ));
   }
 }
