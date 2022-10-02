ba15f893b5d22dbbeda3cbcf62ade07e3ca195ea
gumberss <gustavomarinsuppi@gmail.com>
2022-10-02 19:21:02 -0300

    feat: adding fields validation

--- lib/purchases/screens/initiation/PurchaseInitiation.dart
+++ lib/purchases/screens/initiation/PurchaseInitiation.dart
@@ -4,15 +4,25 @@ import 'package:flutter/material.dart';
 
 import '../../domain/PurchaseList.dart';
 
-class PurchaseInitiation extends StatelessWidget {
+class PurchaseInitiation extends StatefulWidget {
   final PurchaseList _purchaseList;
+
+  final _formKey;
 
   PurchaseInitiation(PurchaseList purchaseList, {Key? key})
       : _purchaseList = purchaseList,
+        _formKey = GlobalKey<FormState>(),
         super(key: key);
 
+  @override
+  State<PurchaseInitiation> createState() => _PurchaseInitiationState();
+}
+
+class _PurchaseInitiationState extends State<PurchaseInitiation> {
   final TextEditingController _placeController = TextEditingController();
+
   final TextEditingController _typeController = TextEditingController();
+
   final TextEditingController _titleController = TextEditingController();
 
   @override
@@ -21,34 +31,50 @@ class PurchaseInitiation extends StatelessWidget {
         appBar: AppBar(
           title: Text(AppLocalizations.of(context)!.startBuying +
               ": " +
-              _purchaseList.name),
+              widget._purchaseList.name),
         ),
         backgroundColor: Colors.grey.shade200,
-        body: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            DefaultInput(AppLocalizations.of(context)!.whereAreYouBuying,
-                TextInputType.text, _placeController,
-                hintText: AppLocalizations.of(context)!.myFavoriteMarket),
-            DefaultInput(
-              AppLocalizations.of(context)!.whatAreYouBuying,
-              TextInputType.text,
-              _typeController,
-              hintText: AppLocalizations.of(context)!.marketShopping,
-            ),
-            DefaultInput(
-              AppLocalizations.of(context)!.purchaseTitle,
-              TextInputType.text,
-              _titleController,
-              hintText: AppLocalizations.of(context)!.weeklyShop,
-            ),
-          ],
+        body: Form(
+          key: widget._formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              DefaultInput(AppLocalizations.of(context)!.whereAreYouBuying,
+                  TextInputType.text, _placeController,
+                  validator: (text) => text == null || text.isEmpty
+                      ? AppLocalizations.of(context)!.mustBeFilled
+                      : null,
+                  hintText: AppLocalizations.of(context)!.myFavoriteMarket),
+              DefaultInput(
+                AppLocalizations.of(context)!.whatAreYouBuying,
+                TextInputType.text,
+                _typeController,
+                validator: (text) => text == null || text.isEmpty
+                    ? AppLocalizations.of(context)!.mustBeFilled
+                    : null,
+                hintText: AppLocalizations.of(context)!.marketShopping,
+              ),
+              DefaultInput(
+                AppLocalizations.of(context)!.purchaseTitle,
+                TextInputType.text,
+                _titleController,
+                validator: (text) => text == null || text.isEmpty
+                    ? AppLocalizations.of(context)!.mustBeFilled
+                    : null,
+                hintText: AppLocalizations.of(context)!.weeklyShop,
+              ),
+            ],
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.blueAccent,
           child: const Icon(Icons.arrow_forward),
-          onPressed: () {},
+          onPressed: () {
+            var isValidForm =
+                (widget._formKey.currentState?.validate() ?? false);
+            if (!isValidForm) return;
+          },
         ));
   }
 }
