8f6c940e4fcd83fd102b609ce69d1c1400d0d289
gumberss <gustavomarinsuppi@gmail.com>
2022-11-18 19:03:20 -0300

    fix: order categories and items by order position

--- lib/components/Immutable.dart
+++ lib/components/Immutable.dart
@@ -1,4 +1,4 @@
-extension ImmutableExtensions<E> on Iterable<E> {
-  Iterable<E> sortedBy(Comparable Function(E e) key) =>
+extension ImmutableExtensions<E> on List<E> {
+  List<E> sortedBy(Comparable Function(E e) key) =>
       toList()..sort((a, b) => key(a).compareTo(key(b)));
 }
